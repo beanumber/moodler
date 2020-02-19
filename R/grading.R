00d1222732ec49d7d4ed9325a5fd9caecee08fb9
Ben Baumer <ben.baumer@gmail.com>
2020-02-19 15:30:25 -0500

    added render_progress()

--- R/grading.R
+++ R/grading.R
@@ -35,3 +35,33 @@ display <- function(x) {
     paste("Discretionary Points:", x$discretionary),
     out)
 }
+
+
+#' Report on compilation in a directory
+#' @param path path to assignment directory
+#' @param ... currently ignored
+#' @export
+#' @examples
+#' dir <- "~/Dropbox/sds192-s20/SDS192- 01_202003-Homework 2 ggplot2-530786"
+#' render_progress(dir)
+
+render_progress <- function(path = ".", ...) {
+  html <- fs::dir_info(path, recurse = TRUE, regexp = "*.html") %>%
+    mutate(parent_dir = fs::path_dir(path)) %>%
+    select(path_html = path, parent_dir)
+  rmd <- fs::dir_info(path, recurse = TRUE, regexp = "*.(R|r)md") %>%
+    mutate(parent_dir = fs::path_dir(path)) %>%
+    select(path_rmd = path, parent_dir)
+
+  x <- full_join(html, rmd, by = "parent_dir") %>%
+    mutate(
+      complete = !is.na(path_html) & !is.na(path_rmd),
+      not_compile = !is.na(path_rmd) & is.na(path_html),
+      not_markdown = is.na(path_rmd) & !is.na(path_html)
+    )
+  message(paste(nrow(filter(x, complete)), "completed."))
+  x %>%
+    filter(!complete) %>%
+    mutate(student = fs::path_file(parent_dir)) %>%
+    select(student, not_compile, not_markdown)
+}
